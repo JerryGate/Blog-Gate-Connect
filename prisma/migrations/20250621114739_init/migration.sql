2acdaad8cfa343e0c06f2f32fe7bb339ec82a58e
JerryGate <jerrydominic11@gmail.com>
2025-06-21 05:00:03 -0700

    add prisma postinstall

--- prisma/migrations/20250621114739_init/migration.sql
+++ prisma/migrations/20250621114739_init/migration.sql
@@ -1,12 +1,14 @@
 -- CreateTable
 CREATE TABLE "BlogPost" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "imageUrl" TEXT NOT NULL,
     "authorId" TEXT NOT NULL,
     "authorName" TEXT NOT NULL,
     "authorImage" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BlogPost_pkey" PRIMARY KEY ("id")
 );
